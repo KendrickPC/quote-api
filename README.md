26650e5d22764b0362525ab5f3a17f94d65a7958
kendrickPC <kennethpchang@gmail.com>
2021-09-06 22:36:47 -0700

    Project needs help with delete call

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
 # README.md
 
+4. Your API should have a GET /api/quotes/random route. This route should send back a random quote from the quotes data. The response body should have the following shape:
+
+The response body should have the following shape:
+```javascript
+{
+  quote: {
+    quote: 'bleh bleh bleh',
+    person: 'Someone BlehBleh',
+  }
+}
+```
+
 5. Your API should have a GET /api/quotes route. This route should return all quotes from the data if the request has no query params.
 
 If there is a query string with a person attribute, the route should return all quotes said by the same person. For instance, the data set has multiple quotes for Grace Hopper, so GET /api/quotes?person=Grace Hopper should return an array of only those quotes. If there are no quotes for the requested person, send back an empty array.
@@ -20,3 +32,20 @@ The response body should have the following shape for all GET /api/quotes reques
 6. Your API should have a POST /api/quotes route for adding new quotes to the data. New quotes will be passed in a query string with two properties: quote with the quote text itself, and person with the person who is credited with saying the quote.
 
 This route should verify that both properties exist in the request query string and send a 400 response if it does not. If all is well, this route handler should add the new quote object to the data array and send back a response with the following shape:
+
+
+6. Your API should have a POST /api/quotes route for adding new quotes to the data. New quotes will be passed in a query string with two properties: quote with the quote text itself, and person with the person who is credited with saying the quote.
+
+This route should verify that both properties exist in the request query string and send a 400 response if it does not. If all is well, this route handler should add the new quote object to the data array and send back a response with the following shape:
+
+```javascript
+{
+  quote: {/* new quote object */}
+}
+```
+
+8. Add a PUT route for updating quotes in the data.
+   This might require adding some sort of unique ID for each quote 
+   in the array in data.js.
+
+
--- server.js
+++ server.js
@@ -9,6 +9,18 @@ const PORT = process.env.PORT || 4001;
 
 app.use(express.static('public'));
 
+// Adding ID to quotes
+for (let i=0; i<quotes.length; i++) {
+  quotes[i] = {
+    id: i,
+    ...quotes[i]
+  }
+}
+// Shows that the ID has been added to the data items.
+// console.log(quotes);
+
+console.log(quotes);
+
 app.get('/api/quotes/random', (req, res, next) => {
   
   const responseBody = {quote: getRandomElement(quotes) }
@@ -20,7 +32,7 @@ app.get('/api/quotes', (req, res, next) => {
   const queryPerson = req.query.person
   const requestedQuotes = [];
   if (!queryPerson) {
-    res.send(quotes);
+    res.send({quotes: quotes});
   } else {
     quotes.forEach(item => {
       if (queryPerson === item.person) {
@@ -31,7 +43,61 @@ app.get('/api/quotes', (req, res, next) => {
   }
 })
 
+app.post('/api/quotes', (req, res, next) => {
+  const queryQuote = req.query.quote;
+  const queryPerson = req.query.person;
+  
+  const responseBody = {
+    quote: {
+      quote: queryQuote,
+      person: queryPerson,
+    }
+  }
 
+  if (queryPerson && queryQuote) {
+    quotes.push( {
+      quote: queryQuote,
+      person: queryPerson,
+    } );
+    res.send(responseBody);
+    console.log(quotes);
+  } else {
+    res.status(400).send("queryPerson or queryQuote properties do not exist.")
+  }
+})
+
+
+
+app.put('/api/quotes/:id', (req, res, next) => {
+  const id = req.params.id;
+  const queryQuote = req.query.quote;
+  const queryPerson = req.query.person;
+  
+  if (id && req.query.person && req.query.quote) {
+    quotes[id] = {
+      id: id,
+      quote: queryQuote,
+      person: queryPerson,
+    }
+    res.status(200).send(quotes[id]);
+  } else {
+    res.status(400).send("SOMETHING WENT WRONG in this PUT")
+  }
+})
+
+app.delete('/api/quotes/:id', (req, res) => {
+  let index = req.params.id;
+  for (let i=0; i<quotes.length; i++) {
+    if (quotes[i].id == index) {
+      quotes.splice(quotes[i].id)
+      console.log(quotes);
+      res.status(200).send("SUCCESS IN DELETEION")
+    } else {
+      res.status(400).send("FAILED IN DELETION")
+    }
+  }
+
+});
 
 app.listen(PORT, () => {
   console.log(`App is listening at ${PORT} because I told it to listen at this PORT # HOMIE!`)
